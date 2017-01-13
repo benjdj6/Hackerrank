a44f40791f1eb70b9adfd56afb02b33c804fa57e
benjdj6 <benjdj6@vt.edu>
2017-01-13 16:56:07 -0500

    Solution for Running Time of Algorithms

---
+++ Algorithms/Sorting/RunTimeofAlgorithms.py
@@ -0,0 +1,16 @@
+def insertionSort(l):
+    shift = 0
+    for i in xrange(1, len(l)):
+        j = i - 1
+        key = l[i]
+        while (j >= 0) and (l[j] > key):
+            l[j + 1] = l[j]
+            j -= 1
+            shift += 1
+        l[j + 1] = key
+    print shift
+
+
+m = input()
+ar = [int(i) for i in raw_input().strip().split()]
+insertionSort(ar)
