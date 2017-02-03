61e27f378b2e3e747e7134882e660f35bdcc8d4e
benjdj6 <benjdj6@vt.edu>
2017-02-02 22:08:40 -0500

    Solution to Check Subset

---
+++ Python/Sets/CheckSubset.py
@@ -0,0 +1,9 @@
+for i in range(int(raw_input())):
+    a = int(raw_input())
+    A = set(raw_input().split())
+    b = int(raw_input())
+    B = set(raw_input().split())
+    if A.issubset(B):
+        print "True"
+    else:
+        print "False"
