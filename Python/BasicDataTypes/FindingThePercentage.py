14731d35392a46aa25f1c550c7ba265893c50f50
benjdj6 <benjdj6@vt.edu>
2017-03-22 20:39:07 -0400

    Solution to Finding the Percentage

---
+++ Python/BasicDataTypes/FindingThePercentage.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(raw_input())
+    student_marks = {}
+    for _ in range(n):
+        line = raw_input().split()
+        name, scores = line[0], line[1:]
+        scores = map(float, scores)
+        student_marks[name] = scores
+    query_name = raw_input()
+    marks = student_marks[query_name]
+    avg = float(sum(marks) / len(marks))
+    print ("{0:.2f}".format(avg))
