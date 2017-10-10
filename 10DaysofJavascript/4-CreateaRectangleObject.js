c7bd276ce3d8699ed2348d965c113ad6e1ac83cb
benjdj6 <benjdj6@vt.edu>
2017-10-09 20:24:42 -0700

    Solution for Day 4: Create a Rectangle Object

---
+++ 10DaysofJavascript/4-CreateaRectangleObject.js
@@ -0,0 +1,11 @@
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    return {
+        length: a,
+        width: b,
+        perimeter: ((2 * a) + (2 * b)),
+        area: (a * b)
+    };
+}
